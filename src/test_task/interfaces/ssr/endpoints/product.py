d94d2af26c797e327d7f80afaa373a7c67d7abb9
berestianyi <berestvania@gmail.com>
2025-09-23 00:45:12 +0300

    add endpoints for product list

---
+++ src/test_task/interfaces/ssr/endpoints/product.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+from dependency_injector.wiring import Provide, inject
+from flask import render_template, Blueprint
+
+from src.test_task.application import ServiceContainer
+from src.test_task.services.product.list.ports import ProductListInput
+from src.test_task.services.product.list.service import ProductListService
+
+HERE = Path(__file__).resolve().parent
+TEMPLATES_DIR = HERE.parent / "templates"
+
+product_bp = Blueprint(
+    "product",
+    __name__,
+    url_prefix="/product",
+    template_folder=str(TEMPLATES_DIR),
+)
+
+
+@product_bp.route("/list", methods=["GET"])
+@inject
+def product_list(
+        product_service: ProductListService = Provide[ServiceContainer.product_service],
+):
+    products = product_service.execute(ProductListInput())
+    return render_template("products.html", products=products)
