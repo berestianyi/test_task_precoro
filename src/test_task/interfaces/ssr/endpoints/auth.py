7f57986a0e73f766b13e0602b92a862ea119137e
berestianyi <berestvania@gmail.com>
2025-09-23 04:16:31 +0300

    remove TEMPLATES_DIR

--- src/test_task/interfaces/ssr/endpoints/auth.py
+++ src/test_task/interfaces/ssr/endpoints/auth.py
@@ -1,18 +1,17 @@
-from pathlib import Path
 
 from dependency_injector.wiring import Provide, inject
 from flask import render_template, redirect, url_for, Blueprint, request
 from flask_login import login_required, logout_user
 
 from src.test_task.application import ServiceContainer, InterfacesContainer
+from src.test_task.interfaces.ssr.endpoints import TEMPLATES_DIR
 from src.test_task.interfaces.ssr.forms.auth import LoginForm, RegisterForm
 from src.test_task.services.user.login.ports import LoginInput
 from src.test_task.services.user.login.service import LoginService
 from src.test_task.services.user.register.ports import RegisterInput
 from src.test_task.services.user.register.service import RegisterService
 
-HERE = Path(__file__).resolve().parent
-TEMPLATES_DIR = HERE.parent / "templates"
+
 
 auth_bp = Blueprint(
     "auth",
